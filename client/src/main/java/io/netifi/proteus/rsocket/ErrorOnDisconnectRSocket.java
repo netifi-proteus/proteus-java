aeb878d244b52b467d08f42a73707546ff38e329
Robert Roeser <rroeserr@gmail.com>
2018-07-23 13:30:05 -0700

    fixed imports

--- client/src/main/java/io/netifi/proteus/rsocket/ErrorOnDisconnectRSocket.java
+++ client/src/main/java/io/netifi/proteus/rsocket/ErrorOnDisconnectRSocket.java
@@ -1,6 +1,5 @@
 package io.netifi.proteus.rsocket;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
