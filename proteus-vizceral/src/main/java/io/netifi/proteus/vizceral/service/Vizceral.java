9c1979fc20f7edadc91bbe7a4fe54c554c83aa3f
Kyle Bahr <kyle@netifi.com>
2018-09-13 16:21:23 -0700

    clean up

--- proteus-vizceral/src/main/java/io/netifi/proteus/vizceral/service/Vizceral.java
+++
@@ -1,9 +0,0 @@
-package io.netifi.proteus.vizceral.service;
-
-import io.netifi.proteus.vizceral.Node;
-import reactor.core.publisher.Flux;
-
-public interface Vizceral {
-
-  Flux<Node> visualisations();
-}
